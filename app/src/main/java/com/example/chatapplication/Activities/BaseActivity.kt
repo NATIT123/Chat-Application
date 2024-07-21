14c023f9b82b88fa26e31fd798c374d474b5a546
Tu <52100138@student.tdtu.edu.vn>
2024-07-21 17:53:39 +0700

    Check Availability User

---
+++ app/src/main/java/com/example/chatapplication/Activities/BaseActivity.kt
@@ -0,0 +1,38 @@
+package com.example.chatapplication.Activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chatapplication.Utils.Constants.Companion.KEY_AVAILABILITY
+import com.example.chatapplication.Utils.Constants.Companion.KEY_COLLECTION_USERS
+import com.example.chatapplication.Utils.Constants.Companion.KEY_USER_ID
+import com.example.chatapplication.Utils.PreferenceManager
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var preferenceManager: PreferenceManager
+    private lateinit var dbRef: DocumentReference
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        preferenceManager = PreferenceManager(applicationContext)
+        preferenceManager.instance()
+        db = FirebaseFirestore.getInstance()
+        dbRef =
+            db.collection(KEY_COLLECTION_USERS).document(preferenceManager.getString(KEY_USER_ID)!!)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dbRef.update(KEY_AVAILABILITY, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dbRef.update(KEY_AVAILABILITY, 0)
+    }
+}
---
+++ app/src/main/java/com/example/chatapplication/Retrofit/ApiService.kt
@@ -0,0 +1,41 @@
+package com.example.chatapplication.Retrofit
+
+import com.example.chatapplication.Utils.Constants.Companion.BASE_URL
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.HeaderMap
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface ApiService {
+    companion object {
+        private val gson: Gson = GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create()
+
+        val apiService: ApiService by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder().addInterceptor(logging).build()
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
+                GsonConverterFactory.create(
+                    gson
+                )
+            ).client(client).build().create(ApiService::class.java)
+        }
+    }
+
+    @POST("most-popular")
+    fun sendMessage(
+        @HeaderMap headers: HashMap<String, String>,
+        @Body messageBody: String
+    ): Call<String>
+
+
+}
